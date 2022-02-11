5c2142afa764ffc965076c731741a0a7124a595d
Sean Fisher <srtfisher@gmail.com>
2022-02-11 14:47:56 -0500

    Add a helper to explain the test

--- tests/AutoloaderTest.php
+++ tests/AutoloaderTest.php
@@ -4,9 +4,19 @@ namespace ComposerWordPressAutoloader\Tests;
 
 use ComposerWordPressAutoloaderTests\Example_Class;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class AutoloaderTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (!file_exists(__DIR__ . '/includes/vendor/wordpress-autoload.php')) {
+            throw new RuntimeException('"composer install" needs to be run in tests/includes');
+        }
+    }
+
     public function testAutoloadedClass()
     {
         // Ensure it is undefined until we load it.
